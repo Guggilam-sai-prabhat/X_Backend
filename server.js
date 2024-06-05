4d568f88003005c87a8b093b230260c6f81e08bc
Prabhat0321 <guggliamprabhat@gmail.com>
2024-06-05 19:33:31 +0530

    modified

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import app from "./index.js"
 
+const PORT =  process.env.PORT || 8080
 app.listen(process.env.port,()=>{
-    console.log(`server is running at port ${process.env.port}`)
+    console.log(`server is running at port ${PORT}`)
 })
