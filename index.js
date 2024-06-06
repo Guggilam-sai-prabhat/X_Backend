cc19b2cbfd2d67f7d4288bf1c3c49c02b80eef28
Guggilam Sai Prabhat <84320979+Guggilam-sai-prabhat@users.noreply.github.com>
2024-06-06 17:33:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({
 app.use(express.json())
 app.use(cookieParser())
 const corsOtions = {
-    origin: true,
+    origin: "https://x-frontend-henna.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }
